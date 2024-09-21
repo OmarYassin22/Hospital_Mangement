d388571ae1f1feba54afac0a17670b5943d3f722
Abanoub_Fawaaz <abanoubfawaz457@gmail.com>
2024-09-22 00:19:04 +0300

    ++

--- Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13ecbbb9e059150a58550a3eaa753560804d87f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d52e1d8b63b7a2395b7c946513e6294577a9f0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
