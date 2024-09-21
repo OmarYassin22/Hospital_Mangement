07a5921b7c2e5228d150db84466bea0f5ba00b1f
Abanoub_Fawaaz <abanoubfawaz457@gmail.com>
2024-09-22 00:37:45 +0300

    Add entity Doctor and Staff

---
+++ Hospitl_Mangement_MVC/Models/Doctor.cs
@@ -0,0 +1,10 @@
+﻿namespace Hospitl_Mangement_MVC.Models
+{
+    public class Doctor :BaseEntity
+    {
+        public string Speciatly { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int Departmentid { get; set; }
+    }
+}
---
+++ Hospitl_Mangement_MVC/Models/Staff.cs
@@ -0,0 +1,10 @@
+﻿namespace Hospitl_Mangement_MVC.Models
+{
+    public class Staff :BaseEntity
+    {
+        public string Role { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int Departmentid { get; set; }
+    }
+}
