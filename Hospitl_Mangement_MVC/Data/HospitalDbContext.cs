3f59c680fb5aa2635adef70843471c4239201c71
Shenoda <shenodaadel63@gmail.com>
2024-09-22 00:55:47 +0300

    addEntityDoctorandStaff

--- Hospitl_Mangement_MVC/Data/HospitalDbContext.cs
+++ Hospitl_Mangement_MVC/Data/HospitalDbContext.cs
@@ -6,6 +6,8 @@ namespace Hospitl_Mangement_MVC.Data
     public class HospitalDbContext :DbContext
     {
         public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctor { get; set; }
+        public DbSet<Staff> Staff { get; set; }
         public HospitalDbContext(DbContextOptions<HospitalDbContext> options) : base(options)
         {
         }
