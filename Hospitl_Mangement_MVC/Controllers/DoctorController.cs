1a8e201db8cfa1cb04f6e19ee4998bde1b8921f5
mohamedsaad21 <mohamedsaad2756@gmail.com>
2024-10-14 23:23:51 +0300

    LoginDropDown

--- Hospitl_Mangement_MVC/Controllers/DoctorController.cs
+++ Hospitl_Mangement_MVC/Controllers/DoctorController.cs
@@ -1,10 +1,12 @@
 ﻿using Hospitl_Mangement_MVC.Data;
 using Hospitl_Mangement_MVC.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Hospitl_Mangement_MVC.Controllers
 {
+    [Authorize(Roles = "Doctor")]
     public class DoctorController : Controller
     {
         private readonly HospitalDbContext _context;
