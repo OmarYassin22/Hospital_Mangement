fef74303a17c8717f6f4d32ede3613e5753a19a2
Shenoda <shenodaadel63@gmail.com>
2024-10-17 21:09:37 +0300

    edit view department management

--- Hospitl_Mangement_MVC/Controllers/DepartmentController.cs
+++ Hospitl_Mangement_MVC/Controllers/DepartmentController.cs
@@ -1,10 +1,12 @@
 ﻿using Hospitl_Mangement_MVC.Data;
 using Hospitl_Mangement_MVC.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Hospitl_Mangement_MVC.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class DepartmentController : Controller
     {
         private readonly HospitalDbContext _context;
--- Hospitl_Mangement_MVC/obj/Debug/net8.0/Hospitl_Mangement_MVC.AssemblyInfo.cs
+++ Hospitl_Mangement_MVC/obj/Debug/net8.0/Hospitl_Mangement_MVC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospitl_Mangement_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f3d61ec903b70c969b1ffbd6a97135775a1fa25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ace777422c4ee67948ab8725c4e3137dcf3fe190")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospitl_Mangement_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospitl_Mangement_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
